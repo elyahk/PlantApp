b4937c81c662ba37fba783c9281abb3ca2e74e62
Bekzod <bekzod.k2407@gmail.com>
2022-07-12 17:36:50 +0500

    added collectionView for example and testing

--- PlantApp/SignUpViewController.swift
+++ PlantApp/SignUpViewController.swift
@@ -7,10 +7,47 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController{
+class SignUpViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    private lazy var testCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        var view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.register(CollectionCell.self, forCellWithReuseIdentifier: "image")
+        view.dataSource = self
+        view.delegate = self
+        return view
+    }()
+    
+    
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray6
+     setupSubviews()
     }
+    
+    private func setupSubviews() {
+        view.addSubview(testCollectionView)
+        
+        NSLayoutConstraint.activate([
+            testCollectionView.topAnchor.constraint(equalTo: view.topAnchor,constant: 25),
+            testCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 24),
+            testCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -25),
+            testCollectionView.bottomAnchor.constraint(equalTo: view.topAnchor,constant: -25),
+        ])
+    }
+}
+
+extension SignUpViewController {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "image", for: indexPath) as? CollectionCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+    
 }
---
+++ PlantApp/TestCollectionCell.swift
@@ -0,0 +1,41 @@
+//
+//  TestCollectionCell.swift
+//  PlantApp
+//
+//  Created by user on 12/07/22.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+    private lazy var cellImage: UIImageView = {
+        var view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFill
+        view.image = (Int.random(in: 0...3) != 0) ? Images.readPageImage.image : Images.learnPageDot.image
+        return view
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubViews()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("error")
+    }
+    
+    private func setupSubViews() {
+        self.addSubview(cellImage)
+        
+        NSLayoutConstraint.activate([
+            cellImage.topAnchor.constraint(equalTo: self.topAnchor),
+            cellImage.leftAnchor.constraint(equalTo: self.leftAnchor),
+            cellImage.rightAnchor.constraint(equalTo: self.rightAnchor),
+            cellImage.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+     
+    
+}
