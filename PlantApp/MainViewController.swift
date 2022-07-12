41ed136fb942937faf4d7e0e82cc9047473fd062
Bekzod <bekzod.k2407@gmail.com>
2022-07-12 22:28:33 +0500

    added skelet with collectionViewCompositionLayout  for MainVC

--- PlantApp/MainViewController.swift
+++ PlantApp/MainViewController.swift
@@ -52,22 +52,59 @@ class MainViewController: UIViewController {
     
     private func setupLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
-            let item = NSCollectionLayoutItem(layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .fractionalHeight(1)))
-            item.contentInsets = .init(top: 3, leading: 3, bottom: 3, trailing: 3)
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .fractionalHeight(1)), subitem: item, count: 1)
-            return NSCollectionLayoutSection(group: group)
+            if sectionIndex == 0 {
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)))
+                item.contentInsets = .init(top: 3, leading: 3, bottom: 3, trailing: 3)
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(0.25)), subitem: item, count: 1)
+                return NSCollectionLayoutSection(group: group)
+            }
+            else if sectionIndex == 1 {
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .absolute(105),
+                    heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 23
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(105)), subitem: item, count: 3)
+                group.contentInsets = .init(top: 35, leading: 23, bottom: 0, trailing: 0.0)
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                return section
+            } else {
+                let item = NSCollectionLayoutItem(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(1)))
+                item.contentInsets.trailing = 23
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .fractionalHeight(0.25)), subitem: item, count: 1)
+                group.contentInsets = .init(top: 35, leading: 23, bottom: 0, trailing: 0.0)
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .continuous
+                return section
+            }
         }
         return layout
     }
 }
 extension MainViewController: UICollectionViewDelegate , UICollectionViewDataSource {
-    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+        
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        if section == 0 {
+            return 1
+        } else if section == 1 {
+            return 3
+        } else if section == 2 {
+            return 3
+        }
+        return 0
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = contectCollectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
